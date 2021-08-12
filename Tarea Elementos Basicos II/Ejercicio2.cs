2cdac67383a1e1c5b4c58b73ab645d07788d4951
Fatima Ayala <ftayala89@gmail.com>
2021-08-12 11:14:56 -0600

    Ejercicios 2

---
+++ Tarea Elementos Basicos II/Ejercicio2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Elementos_Basicos_II
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            //escribir un ciclo que permita capturar los valores
+            //almacenar en el arreglo de numbers con un tamaño de 10
+
+            int[] nume = new int[10];
+            int sumat = 0;
+
+            for (int f = 0; f < nume.Length; f++)
+            {
+                Console.WriteLine("Introduzca un numero");
+                nume[f] = Convert.ToInt32(Console.ReadLine());
+                sumat = sumat + nume[f];
+            }
+            Console.WriteLine("Los numeros que usten ingreso son:");
+            foreach (int item in nume)
+            {
+                Console.WriteLine(item);
+                
+
+            }
+            Console.WriteLine("La sumatoria de los numeros que usted ingreso es: {0} ", sumat);
+            Console.WriteLine("El promedio de los numeros que usted ingreso es: {0}", sumat / (nume.Length));
+            Console.ReadKey();
+        }
+    }
+}
