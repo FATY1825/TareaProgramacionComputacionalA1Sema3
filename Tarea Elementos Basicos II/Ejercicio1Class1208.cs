6b6ec5d92a3150c9d0f97a1dfb60c1f374984c85
Fatima Ayala <ftayala89@gmail.com>
2021-08-12 08:40:08 -0600

    Agregar archivos de proyecto.

---
+++ Tarea Elementos Basicos II/Ejercicio1Class1208.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Elementos_Basicos_II
+{
+    class Ejercicio1Class1208
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
